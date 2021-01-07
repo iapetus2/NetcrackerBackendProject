47bb06567dbbd21fd40d687ae8681046499b9c81
st256-ai <62150678+st256-ai@users.noreply.github.com>
2021-01-07 19:13:00 +0300

    Add files via upload

---
+++ src/main/java/com/projectparty/service/DealService.java
@@ -0,0 +1,17 @@
+package com.projectparty.service;
+
+import com.projectparty.entities.Deal;
+
+import java.util.List;
+
+public interface DealService {
+    void save(Deal deal);
+
+    List<Deal> readAll();
+
+    Deal read(int id);
+
+    boolean update(Deal deal, int id);
+
+    boolean delete(int id);
+}
