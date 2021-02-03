d080766541aff968dbb3c8204b9fdc4aab68c9dc
st256-ai <62150678+st256-ai@users.noreply.github.com>
2021-02-03 20:52:03 +0300

    Update MessageController.java

--- src/main/java/com/projectparty/controllers/MessageController.java
+++ src/main/java/com/projectparty/controllers/MessageController.java
@@ -1,17 +1,20 @@
 package com.projectparty.controllers;
-import com.projectparty.graphs.Graph;
-import com.projectparty.graphs.GraphMessage;
-import org.hibernate.HibernateException;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 
-@Controller
+import com.projectparty.graphs.GraphMessage;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageController {
 
-    @EventListener
-    @SendTo("/topic/messages")
-    public void send(GraphMessage graphMessage) throws HibernateException {
-        new Graph(graphMessage.getDeal());
+    private final SimpMessagingTemplate template;
+
+    public MessageController(SimpMessagingTemplate template) {
+        this.template = template;
     }
+
+    public void send(GraphMessage graphMessage) {
+        template.convertAndSend("/topic/messages", graphMessage);
+    }
+
 }
