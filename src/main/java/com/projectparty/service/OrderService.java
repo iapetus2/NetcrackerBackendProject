2b1d27e0f70758532576acfe24fc1e2c3dcf1ef6
st256-ai <62150678+st256-ai@users.noreply.github.com>
2021-02-28 01:42:39 +0300

    Update OrderService.java

--- src/main/java/com/projectparty/service/OrderService.java
+++ src/main/java/com/projectparty/service/OrderService.java
@@ -1,18 +1,21 @@
 package com.projectparty.service;
 
 import com.projectparty.entities.Order;
+
 import java.util.List;
 
 public interface OrderService {
-
     void save(Order order);
 
     List<Order> readAll();
+
+    List<Order> readAllItems(int itemId);
 
     Order read(int id);
 
     boolean update(Order order, int id);
 
-    boolean delete(int id);
+     List<Order> findMatchingOrders(Order order);
 
+    boolean delete(int id);
 }
