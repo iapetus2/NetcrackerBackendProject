4a9a6f9d12962cf2ae05e1f79f1ef8ce1fcc5c65
st256-ai <62150678+st256-ai@users.noreply.github.com>
2021-02-28 01:32:48 +0300

    Update OrderDao.java

--- src/main/java/com/projectparty/dao/OrderDao.java
+++ src/main/java/com/projectparty/dao/OrderDao.java
@@ -7,35 +7,48 @@ import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import org.apache.log4j.Logger;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class OrderDao {
     private static final Logger logger = Logger.getLogger(OrderDao.class.getName());
 
-    public void save(Order order) {
-        try {
-            Session session = HibernateSessionFactoryUtil
-                    .getSessionFactory()
-                    .openSession();
+    public void save(Order Order) {
+        try (var session = HibernateSessionFactoryUtil
+                .getSessionFactory()
+                .openSession()) {
             Transaction transaction = session
                     .beginTransaction();
-            session.save(order);
-            transaction.commit();
-            session.close();
-        }catch (RuntimeException e){
-            logger.error("Order creating failure");
+            try {
+                session.save(Order);
+                transaction.commit();
+            } catch (final Exception e) {
+                transaction.rollback();
+            }
+        } catch (Exception e){
+            logger.log(Level.SEVERE, "Exception: ", e);
         }
 
     }
 
     public List<Order> readAll() {
-        return HibernateSessionFactoryUtil
+        try(var session = HibernateSessionFactoryUtil
                 .getSessionFactory()
-                .openSession()
-                .createQuery("FROM Order", Order.class)
-                .list();
+                .openSession()) {
+            return session.createQuery("FROM Order", Order.class)
+                    .getResultList();
+        }
+    }
+
+    public List<Order> readAllItems(int id) {
+        try(var session = HibernateSessionFactoryUtil
+                .getSessionFactory()
+                .openSession()) {
+            return session.createQuery("FROM Order WHERE tradingItemId = :itemId", Order.class)
+                    .setParameter("itemId",id)
+                    .list();
+        }
     }
 
     public Order read(int id) {
@@ -45,7 +58,7 @@ public class OrderDao {
                 .get(Order.class, id);
     }
 
-    public boolean update(Order order, int id) {
+    public boolean update(Order Order, int id) {
         try{
             Session session = HibernateSessionFactoryUtil
                     .getSessionFactory()
@@ -53,11 +66,11 @@ public class OrderDao {
             session.load(Order.class, id);
             Transaction transaction = session
                     .beginTransaction();
-            session.update(order);
+            session.update(Order);
             transaction.commit();
             session.close();
-        }catch (RuntimeException e){
-            logger.error("Order updating failure");
+        }catch (Exception e){
+            throw new RuntimeException("Update failure");
         }
 
         return true;
@@ -75,8 +88,8 @@ public class OrderDao {
             session.delete(proxyOrder);
             transaction.commit();
             session.close();
-        }catch (RuntimeException e){
-            logger.error("Order deleting failure");
+        } catch (Exception e) {
+            throw new RuntimeException("Delete failure");
         }
 
         return true;
