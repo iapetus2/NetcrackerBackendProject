9cc7c0e67ce6f2c96ef04a043d2ff44a7d95b0c8
st256-ai <62150678+st256-ai@users.noreply.github.com>
2021-02-28 01:36:12 +0300

    Update Order.java

--- src/main/java/com/projectparty/entities/Order.java
+++ src/main/java/com/projectparty/entities/Order.java
@@ -1,15 +1,8 @@
 package com.projectparty.entities;
 
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -19,14 +12,15 @@ import java.util.Date;
 @Table(name = "Orders")
 public class Order {
 
-    @NonNull
     @Id
-    @Column(name = "id")
-    private int orderId;
-
     @NonNull
-    @Column(name = "tradingItemId")
-    private int tradingItemId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "orderId")
+    private Integer orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "tradingItemId")
+    private TradingItem tradingItem;
 
     @NonNull
     @Column(name = "type")
@@ -36,15 +30,83 @@ public class Order {
     private long orderPrice;
 
     @NonNull
+    @Temporal(TemporalType.DATE)
     @Column(name = "creationDate")
     private Date orderDate;
 
     @NonNull
-    @Column(name = "orderAmount")
-    private int orderAmount;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
 
     @NonNull
-    @Column(name = "userId")
-    private int userId;
+    @Column(name = "amount")
+    private int amount;
 
+    public TradingItem getTradingItem() {
+        return tradingItem;
+    }
+
+    public OrderType getOrderType() {
+        return orderType;
+    }
+
+    public long getOrderPrice() {
+        return orderPrice;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setTradingItem(TradingItem tradingItem) {
+        this.tradingItem = tradingItem;
+    }
+
+    public void setOrderType(OrderType orderType) {
+        this.orderType = orderType;
+    }
+
+    public void setOrderPrice(long orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @PrePersist
+    public void onCreate() {
+        if (orderPrice <= 0 || amount <= 0) {
+            throw new RuntimeException("Both price and amount must be positive");
+        }
+
+        if (orderType == OrderType.SELL && user.getItems().get(tradingItem.getItemId()) < amount) {
+            throw new RuntimeException("Client doesn't have enough items to trade");
+        }
+
+        if (user.getCash() < orderPrice * amount) {
+            throw new RuntimeException("Insufficient funds");
+        }
+    }
 }
